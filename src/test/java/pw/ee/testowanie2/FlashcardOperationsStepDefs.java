81644a71767ef2b57a60d145c75e3bd805985d39
Arkadiusz Ciepliński <arkadiusz.cieplinski2@gmail.com>
2023-11-27 12:53:31 +0100

    Add some step defs for flashcard and new feature files

--- src/test/java/pw/ee/testowanie2/FlashcardOperationsStepDefs.java
+++ src/test/java/pw/ee/testowanie2/FlashcardOperationsStepDefs.java
@@ -1,19 +1,37 @@
 package pw.ee.testowanie2;
 
-import com.google.gson.Gson;
-import io.cucumber.java.en.And;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import pw.ee.testowanie2.models.Flashcard;
-import pw.ee.testowanie2.models.FlashcardDTO;
-import pw.ee.testowanie2.models.Set;
 
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import pw.ee.testowanie2.models.Flashcard;
 
 public class FlashcardOperationsStepDefs extends SpringIntegrationTest {
+
+    @Given("There is an existing Flashcard with the specified id of {int}")
+    public void thereIsAnExistingSetWithNameOf(int arg0) {
+        if (!flashcardRepository.existsById((long) arg0)) {
+            Flashcard flashcardEntity = Flashcard.builder()
+                    .id((long) arg0)
+                    .build();
+            flashcardRepository.save(flashcardEntity);
+        }
+    }
+
+    @When("User makes a DELETE request to {string}")
+    public void userMakesAGETRequestTo(String arg0) throws IOException {
+        executeDelete(arg0);
+    }
+
+    @Then("The response status should be {int}")
+    public void theResponseStatusShouldBe(int arg0) {
+        assertEquals(arg0, latestResponse.statusCode());
+    }
+
     @Given("there is a set with the name {string}")
     public void thereIsASetWithTheName(String arg0) {
         setRepository.save(Set.builder()
