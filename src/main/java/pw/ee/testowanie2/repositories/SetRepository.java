aa5843e967dafd2807dffbfcc8879072867c87f5
Sebastian Pawliński <pawlinskisebastian@gmail.com>
2023-11-27 19:46:39 +0100

    add findByName to SetRepository

--- src/main/java/pw/ee/testowanie2/repositories/SetRepository.java
+++ src/main/java/pw/ee/testowanie2/repositories/SetRepository.java
@@ -3,6 +3,10 @@ package pw.ee.testowanie2.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pw.ee.testowanie2.models.Set;
 
+import java.util.Optional;
+
 public interface SetRepository extends JpaRepository<Set, Long> {
     boolean existsByName(String name);
+
+    Optional<Set> findByName(String name);
 }
