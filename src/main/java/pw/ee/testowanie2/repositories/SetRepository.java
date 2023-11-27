16738d1102263382f670358ae5e84fe5664df1fd
szlazakm <01169634@pw.edu.pl>
2023-11-27 21:11:14 +0100

    SetRepository methods for GET requests and pom.xml cleanup

--- src/main/java/pw/ee/testowanie2/repositories/SetRepository.java
+++ src/main/java/pw/ee/testowanie2/repositories/SetRepository.java
@@ -1,8 +1,17 @@
 package pw.ee.testowanie2.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pw.ee.testowanie2.models.Set;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface SetRepository extends JpaRepository<Set, Long> {
+    Optional<Set> findByName(String name);
+    List<Set> findAllByOrderByName();
+    List<Set> findAllByOrderByCreatedAt();
+    @Query("SELECT s FROM Set s LEFT JOIN s.flashcards f GROUP BY s ORDER BY COUNT(f) DESC")
+    List<Set> findAllByOrderByFlashcards();
     boolean existsByName(String name);
 }
