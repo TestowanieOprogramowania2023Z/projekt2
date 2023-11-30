85b145b054a6207a7a82a5c1fa1ea988aeefaceb
Arkadiusz Ciepliński <arkadiusz.cieplinski2@gmail.com>
2023-11-30 13:59:21 +0100

    Add implementation for scenarios

--- src/main/java/pw/ee/testowanie2/services/FlashcardService.java
+++ src/main/java/pw/ee/testowanie2/services/FlashcardService.java
@@ -1,20 +1,23 @@
 package pw.ee.testowanie2.services;
 
+import java.util.NoSuchElementException;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 import pw.ee.testowanie2.models.Flashcard;
+import pw.ee.testowanie2.models.FlashcardCreateDTO;
 import pw.ee.testowanie2.models.FlashcardDTO;
 import pw.ee.testowanie2.repositories.FlashcardRepository;
 
 import java.util.List;
+import pw.ee.testowanie2.repositories.SetRepository;
 
 @Service
 @AllArgsConstructor
 public class FlashcardService {
     private final FlashcardRepository flashcardRepository;
-
+    private final SetRepository setRepository;
     public List<FlashcardDTO> getFlashcardsBySetName(String name) {
         if (name == null) throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Set name cannot be null");
         List<Flashcard> flashcards = flashcardRepository.findBySetName(name);
@@ -28,5 +31,23 @@ public class FlashcardService {
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Flashcard not found"));
         return FlashcardDTO.fromFlashcard(flashcard);
     }
+
+    public void deleteById(Long id) {
+        if (!flashcardRepository.existsById(id)) {
+            throw new NoSuchElementException("Flashcard with provided id doesn't exist");
+        }
+        flashcardRepository.deleteById(id);
+    }
+
+    public void updateById(Long id, FlashcardCreateDTO flashcardCreateDTO) {
+        Flashcard flashcard = flashcardRepository.findById(id)
+            .orElseThrow(() -> new NoSuchElementException("Flashcard with provided id doesn't exist"));
+        flashcard.setSet(setRepository.findById(flashcardCreateDTO.getSetId())
+            .orElseThrow(() -> new NoSuchElementException("Set with provided id doesn't exist")));
+        flashcard.setBack(flashcardCreateDTO.getBack());
+        flashcard.setFront(flashcardCreateDTO.getFront());
+        flashcardRepository.save(flashcard);
+    }
+
 
 }
