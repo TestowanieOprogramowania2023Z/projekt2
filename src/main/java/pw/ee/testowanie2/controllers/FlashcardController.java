abfd9897ab9a1d45e163ba1a2966ff3c94fdda0e
Maciej Kwiatkowski <maciek-kwiatkowski@o2.pl>
2023-11-30 11:44:39 +0100

    Added FlashcardController and service

--- src/main/java/pw/ee/testowanie2/controllers/FlashcardController.java
+++ src/main/java/pw/ee/testowanie2/controllers/FlashcardController.java
@@ -1,8 +1,14 @@
 package pw.ee.testowanie2.controllers;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pw.ee.testowanie2.models.FlashcardCreateDTO;
 import pw.ee.testowanie2.services.FlashcardService;
 
 @RestController
@@ -10,4 +16,13 @@ import pw.ee.testowanie2.services.FlashcardService;
 @AllArgsConstructor
 public class FlashcardController {
     private final FlashcardService flashcardService;
+    @PostMapping(consumes = {"application/json"})
+    public ResponseEntity<FlashcardCreateDTO> createFlashcard(@Valid @RequestBody FlashcardCreateDTO flashcardCreateDTO) {
+        try {
+            flashcardService.createFlashcard(flashcardCreateDTO);
+            return new ResponseEntity<>(flashcardCreateDTO, HttpStatus.CREATED);
+        } catch (IllegalAccessException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
 }
--- src/main/java/pw/ee/testowanie2/services/FlashcardService.java
+++ src/main/java/pw/ee/testowanie2/services/FlashcardService.java
@@ -2,10 +2,27 @@ package pw.ee.testowanie2.services;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import pw.ee.testowanie2.models.Flashcard;
+import pw.ee.testowanie2.models.FlashcardCreateDTO;
 import pw.ee.testowanie2.repositories.FlashcardRepository;
+import pw.ee.testowanie2.repositories.SetRepository;
 
 @Service
 @AllArgsConstructor
 public class FlashcardService {
     private final FlashcardRepository flashcardRepository;
+
+    private final SetRepository setRepository;
+    public Flashcard createFlashcard(FlashcardCreateDTO flashcardCreateDTO) throws IllegalAccessException {
+        if(!setRepository.existsById(flashcardCreateDTO.getSetId()))
+            throw new IllegalAccessException("The set of given id doesnt exist in database");
+
+        Flashcard newFlashcard = Flashcard.builder()
+                .back(flashcardCreateDTO.getBack())
+                .front(flashcardCreateDTO.getFront())
+                .set(setRepository.getById(flashcardCreateDTO.getSetId()))
+                .build();
+
+        return flashcardRepository.save(newFlashcard);
+    }
 }
